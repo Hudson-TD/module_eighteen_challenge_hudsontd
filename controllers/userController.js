8cf3363fe73e3acae977212759f029bd8dca4643
Tyler Hudson <tylerhudson96@gmail.com>
2022-11-06 15:12:09 -0500

    update user route

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,23 +4,36 @@ module.exports = {
   // Get all users
   getUsers(req, res) {
     User.find()
-      .then((usersData) => res.json(usersData))
+      .then((users) => res.json(users))
       .catch((err) => res.status(500).json(err));
   },
   // Get one specific user
   getSingleUser(req, res) {
     User.findOne({ _id: req.params.userId })
-      .then((userData) =>
-        !userData
+      .then((user) =>
+        !user
           ? res.status(404).json({ message: "No user found with this ID" })
-          : res.json(userData)
+          : res.json(user)
       )
       .catch((err) => res.status(500).json(err));
   },
-  // Create new user   
+  // Create new user
   createUser(req, res) {
     User.create(req.body)
       .then((newUser) => res.json(newUser))
+      .catch((err) => res.status(500).json(err));
+  },
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user found with this ID" })
+          : res.json(user)
+      )
       .catch((err) => res.status(500).json(err));
   },
 };
