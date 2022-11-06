1055fa87fd265b3360d8830ae2c92125e40f2474
Tyler Hudson <tylerhudson96@gmail.com>
2022-11-06 16:04:36 -0500

    moved Reaction schema into its own file and exported/imported it into Thought

---
+++ models/Reaction.js
@@ -0,0 +1,24 @@
+const { Schema, Types } = require("mongoose");
+// Need to implement a helper function to pass in to the getter to format date TODO
+// add get: funcName to createdAt object
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = reactionSchema;
--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const reactionSchema = require("./Reaction");
 
 // Need to implement a helper function to pass in to the getter to format date TODO
 // add get: funcName to createdAt object
@@ -18,7 +19,7 @@ const thoughtSchema = new Schema(
       type: String,
       required: true,
     },
-    // reactions: [reactionSchema],
+    reactions: [reactionSchema],
   },
   {
     toJSON: {
@@ -28,28 +29,6 @@ const thoughtSchema = new Schema(
     id: false,
   }
 );
-
-// Need to implement a helper function to pass in to the getter to format date TODO
-// add get: funcName to createdAt object
-const reactionSchema = new Schema({
-  reactionId: {
-    type: Schema.Types.ObjectId,
-    default: () => new Types.ObjectId(),
-  },
-  reactionBody: {
-    type: String,
-    required: true,
-    maxlength: 280,
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
 
 const Thought = model("Thought", thoughtSchema);
 
