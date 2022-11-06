27b28d308ac3fbbd8f5cc76e18c9c8438c16eef2
Tyler Hudson <tylerhudson96@gmail.com>
2022-11-06 15:04:39 -0500

    get one specific user route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,20 @@
 const { Thought, User } = require("../models");
 
 module.exports = {
+  // Get all users
   getUsers(req, res) {
     User.find()
       .then((usersData) => res.json(usersData))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Get specific user
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .then((userData) =>
+        !userData
+          ? res.status(404).json({ message: "No user found with this ID" })
+          : res.json(userData)
+      )
       .catch((err) => res.status(500).json(err));
   },
 };
