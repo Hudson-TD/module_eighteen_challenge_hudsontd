2e09156d19a45ab33ecb15aeaeba70c8162f2836
Tyler Hudson <tylerhudson96@gmail.com>
2022-11-10 17:43:48 -0500

    minor cleanup & renaming of index.js to server.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
-// const thoughtRoutes = require("./api/thoughtRoutes");
 
 router.use("/api", apiRoutes);
-// router.use("/thoughts", thoughtRoutes);
 
 module.exports = router;
