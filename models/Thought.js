68fe309d06a7bf347f56c8b70b476f8bb706b8ba
Tyler Hudson <tylerhudson96@gmail.com>
2022-11-10 17:53:18 -0500

    added virtual for reactions count/value

--- models/Thought.js
+++ models/Thought.js
@@ -54,6 +54,11 @@ const reactionSchema = new Schema({
   },
 });
 
+// Pulling value of reactions array not stored in MongoDB
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
 const Thought = model("Thought", thoughtSchema);
 
 module.exports = Thought;
