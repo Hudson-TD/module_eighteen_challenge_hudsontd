2258a97abfbe6c558bb298ed983a7f46ac030835
Tyler Hudson <tylerhudson96@gmail.com>
2022-11-10 17:55:07 -0500

    moved reaction schema up in JS so it could initialize before being referenced, bug fix

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,29 @@
 const { Schema, model, Types } = require("mongoose");
 const moment = require("moment");
 
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    // moment npm for timestamp formatting
+    get: (createdAtVal) =>
+      moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+  },
+});
+
 const thoughtSchema = new Schema(
   {
     thoughtText: {
@@ -30,29 +53,6 @@ const thoughtSchema = new Schema(
     id: false,
   }
 );
-
-const reactionSchema = new Schema({
-  reactionId: {
-    type: Schema.Types.ObjectId,
-    default: () => new Types.ObjectId(),
-  },
-  reactionBody: {
-    type: String,
-    required: true,
-    maxlength: 280,
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    // moment npm for timestamp formatting
-    get: (createdAtVal) =>
-      moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
-  },
-});
 
 // Pulling value of reactions array not stored in MongoDB
 thoughtSchema.virtual("reactionCount").get(function () {
